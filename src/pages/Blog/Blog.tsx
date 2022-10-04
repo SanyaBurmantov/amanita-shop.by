5041e6be8e1d1cab4a08d89b675f73b6a278eba2
Karina <vbank322@mail.ru>
2022-10-04 22:39:56 +0300

    Filter finished

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -3,7 +3,6 @@ import './Blog.scss'
 import {Post} from "./components/Post/Post";
 import {Best} from "./components/Best/Best";
 import {posts} from "../../data/Posts";
-import {Filter} from "./components/Filter/Filter";
 
 interface Props {
 
@@ -22,6 +21,9 @@ export const Blog = () => {
         }
     }
 
+    const FillArr: {[Themes: string]: number} = {};
+    const fill = posts.filter(({Themes}) => (!FillArr[Themes] && (FillArr[Themes] = 1)));
+
     return (
         <div className='background__blog'>
             <div className='container'>
@@ -30,16 +32,10 @@ export const Blog = () => {
                     <div className='content__posts'>{filtred.map((post) => <Post post={post} key={post.id}/>)}</div>
                     <div className='content__panel'>
                         <div className='panel__filter'>
-                            <div className='filter__text'>Отфильтрованные кнопки тематики из json посты не фильтруются</div>
-                            <Filter />
-                            <div className='filter__texts'>Написанные тематики от балды но по ним посты фильтруются</div>
+                            <div className='filter__text'>Фильтр по тематике</div>
                             <div className='filter__btn'>
                                 <button onClick={() => ArrFilter('all')}>Все</button>
-                                <button onClick={() => ArrFilter('Zalupa')}>Категория 1</button>
-                                <button onClick={() => ArrFilter('Jopa')}>Категория 2</button>
-                                <button onClick={() => ArrFilter('Govno')}>Категория 3</button>
-                                <button onClick={() => ArrFilter('Pizda')}>Категория 4</button>
-                                <button onClick={() => ArrFilter('Hyi')}>Категория 5</button>
+                                <>{fill.map((FillArr) =><button onClick={() => ArrFilter(FillArr.Themes)}>{FillArr.Themes}</button>)}</>
                             </div>
                         </div>
                         <div className='panel__best'><Best/></div>
--- src/pages/Blog/components/Filter/Filter.tsx
+++ src/pages/Blog/components/Filter/Filter.tsx
@@ -1,15 +1,12 @@
-import React, {useState} from 'react';
-import {posts} from '../../../../data/Posts'
+import React from 'react';
 
 interface Props {
 }
 
 export const Filter = () => {
 
-    const FillArr: {[Themes: string]: number} = {};
-    const fill = posts.filter(({Themes}) => (!FillArr[Themes] && (FillArr[Themes] = 1)));
 
     return (
-        <>{fill.map((FillArr) =><button> {FillArr.Themes}</button>)}</>
+      <></>
     );
 };
