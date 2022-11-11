b53f5e5c489af3b718f6f12e09220e93e121e62a
Alexandr <31419907+SanyaBurmantov@users.noreply.github.com>
2022-11-11 13:47:44 +0300

    micro fix

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,10 @@
-import {BLOG_ROUTE, HOME_ROUTE, PAYMENT_ROUTE, PRODUCTS_ROUTE, REVIEWS_ROUTE} from "./utils/consts";
+import {BLOG_ROUTE, HOME_ROUTE, PAYMENT_ROUTE, PRODUCTS_ROUTE, REVIEWS_ROUTE, TELEGRAM_ROUTE} from "./utils/consts";
 import {Home} from "./pages/Home/Home";
 import {Blog} from "./pages/Blog/Blog";
 import {Reviews} from "./pages/Reviews/components/Reviews";
 import {PaymentAndDelivery} from "./pages/Payment-and-delivery/components/Payment-and-delivery";
 import Products from "./pages/Products/components/Products";
+import {Telegram} from "./pages/Telegram/Telegram";
 
 export const publicRoutes = [
     {
@@ -21,7 +22,11 @@ export const publicRoutes = [
     },{
         path: PAYMENT_ROUTE,
         Component: PaymentAndDelivery
+    },{
+        path: TELEGRAM_ROUTE,
+        Component: Telegram,
     }
+
 ]
 
 
