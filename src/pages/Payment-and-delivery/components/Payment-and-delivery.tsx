5560f7389cc3da0ddc662bfa3487f800be4c6072
Aliaksandr Burmantau <aburmantov@fil-it.ru>
2022-11-12 15:28:51 +0300

    ahaha

--- src/pages/Payment-and-delivery/components/Payment-and-delivery.tsx
+++ src/pages/Payment-and-delivery/components/Payment-and-delivery.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import {motion} from "framer-motion";
+import {IRewiews} from "../../../types";
+import {ReviewsItem} from "../../Reviews/components/ReviewsItem/ReviewsItem";
+import {ReviewsModal} from "../../Reviews/components/ReviewsModal/ReviewsModal";
 
 interface Props {
 }
+
 export const PaymentAndDelivery = (props: Props) => {
     return (
-        <motion.div
-            initial={{width: "30%"}}
-            animate={{width: "100%"}}
-            exit={{x: -window.innerWidth, transition: {duration: 0.3}}}>
-            Payment and delivery
+        <motion.div className="section section-delivery"
+                    initial={{width: "30%"}}
+                    animate={{width: "100%"}}
+                    exit={{x: -window.innerWidth, transition: {duration: 0.3}}}>
+            <div className='container'>
+                <div className='reviews-title'>Оплата и доставка</div>
+                <div className='reviews-desc'>
+                    <div className='reviews-desc-subtitle'>Писька жопа писька жопа Писька жопа писька жопа Писька жопа
+                        писька жопа Писька жопа писька жопа Писька жопа писька жопа
+
+                    </div>
+
+                </div>
+            </div>
         </motion.div>
     )
 }
