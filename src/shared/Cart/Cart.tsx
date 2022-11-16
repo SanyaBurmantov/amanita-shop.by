668954a66a7526c17c16d5fc65e678ad1f7f4193
Alexandr <31419907+SanyaBurmantov@users.noreply.github.com>
2022-11-16 20:00:52 +0300

    telega

--- src/shared/Cart/Cart.tsx
+++ src/shared/Cart/Cart.tsx
@@ -1,4 +1,4 @@
-import {FC, useCallback, useEffect, useState} from "react";
+import React, {FC, useCallback, useEffect, useState} from "react";
 import {useTypedSelector} from "../../hooks/useTypedSelector";
 import {useDispatch} from "react-redux";
 import {removeFromCart} from "../../store/cart/actions";
@@ -8,6 +8,10 @@ import Form from "./Form";
 import {useTelegram} from "../../hooks/useTelegram";
 
 export const Cart:FC = () => {
+    const [name, setName] = useState('');
+
+    const [number, setNumber] = useState('');
+
     const {tg, queryId} = useTelegram();
 
     const [isShowCart, setIsShowCart] = useState(false);
@@ -18,6 +22,37 @@ export const Cart:FC = () => {
         acc + item.price * item.count
 
     , 0)
+
+
+
+
+
+    //ts-ignore
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные '
+        })
+    }, [])
+
+    //ts-ignore
+    useEffect(()=> {
+        if(!name || !number){
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [name, number])
+
+
+
+    const onChangeName = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+        setName(e.target.value)
+    }
+
+    const onChangeNumber = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+        setNumber(e.target.value)
+    }
+
 
     const dispatch = useDispatch()
 
@@ -32,6 +67,8 @@ export const Cart:FC = () => {
 
     const onSendData = useCallback(() => {
         const data = {
+            name,
+            number,
             products: cart,
             totalPrice: total,
             queryId,
@@ -44,7 +81,7 @@ export const Cart:FC = () => {
                 text: `Купить ${total}`
             })
         }
-
+        tg.sendData(JSON.stringify(data));
         fetch('http://85.119.146.179:8000/web-data', {
             method: 'POST',
             headers: {
@@ -52,7 +89,7 @@ export const Cart:FC = () => {
             },
             body: JSON.stringify(data)
         })
-    }, [cart])
+    }, [cart, name, number])
 
 
 
@@ -73,7 +110,9 @@ export const Cart:FC = () => {
                     <div className="cart__picture--text">{cart.length}</div>
                 </div>
             </div>
+
             <div className={isShowCart ? 'cart__data active' : 'cart__data'} onClick={() => setIsShowCart(false)}>
+
                 <div className={isShowCart ? 'cart__data--content active' : 'cart__data--content'} onClick={e => e.stopPropagation()}>
                     {cart.map(item => (
                         <div className="cart__data--elem" key={`cart item ${item.name}`}>
@@ -87,13 +126,29 @@ export const Cart:FC = () => {
                         </div>
 
                     ))}
+                    <div>
                     <div className="btn" onClick={onSendData}>Купить </div>
                     <div className="cart__total">
                         Общая сумма: <b>{Math.round(total)}</b>
                     </div>
+                   </div>
+                    <div className="form">
+                        <h3>Введите ваши данные</h3>
+                        <input className='input'
+                               type='text'
+                               placeholder='Имя'
+                               value={name}
+                               onChange={onChangeName} />
+                        <input className='input'
+                               type='text'
+                               placeholder='Номер телефона'
+                               value={number}
+                               onChange={onChangeNumber} />
+                    </div>
                 </div>
 
             </div>
+
         </div>
     )
 }
