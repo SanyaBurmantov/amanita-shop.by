56f241c9acd1da615d204ee800039bdbae723266
sanya <sanyaburmantov@gmail.com>
2022-11-28 05:53:06 +0300

    аппрол

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import {motion} from "framer-motion";
+// @ts-ignore
+import mainpic from '../../assets/page/mainpic.png'
+// @ts-ignore
+import home_img from '../../assets/page/home_img.png'
 
 interface Props {
 
@@ -14,86 +18,58 @@ export const Home = () => {
             animate={{opacity: 1}}
             exit={{x: -window.innerWidth, transition: {duration: 0.2}}}>
             <div className="container">
+                <div className="back"></div>
+                <div className="heading">
+                    <div className="heading__picture">
+                        <img src={mainpic} alt='picture'/>
+                    </div>
+                </div>
+                <div className="promo">
+                    <div className="promo__text">
+                        <h1>Дары природы</h1>
+                        <p>Польза для здоровья каждый день</p>
+                        <div className="promo__button">
+                            <button className="btn">Переийти в каталог</button>
+                        </div>
+                    </div>
 
-                <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias assumenda eius eligendi, expedita
-                    iusto natus nesciunt nobis. Deserunt, dolorum eos explicabo nisi perspiciatis qui reiciendis
-                    reprehenderit sit soluta. A ab ad amet animi asperiores beatae commodi debitis delectus ea eaque
-                    eius enim eos exercitationem id illo ipsa iste itaque labore laborum libero, magnam modi nisi nobis
-                    numquam odit officia pariatur quas quibusdam quo quod ratione repellendus sint soluta tempora
-                    temporibus velit voluptates? Accusamus aut, doloribus ducimus eaque eveniet odit officia porro quia
-                    tempora? Aliquam aperiam architecto commodi consectetur consequuntur, cupiditate debitis dicta,
-                    dolore eaque earum hic illo inventore ipsa laboriosam maxime molestiae necessitatibus nobis nostrum
-                    officiis possimus provident quia quo quod rem, reprehenderit saepe sequi similique suscipit tenetur
-                    vel veniam voluptas voluptate voluptatum? Accusantium animi dolor ducimus exercitationem iure iusto
-                    laborum laudantium nemo nobis, officia quas saepe sapiente sunt unde veniam. Autem corporis earum
-                    quae saepe vitae. A accusantium aspernatur assumenda atque aut consequuntur cupiditate eius error et
-                    expedita id impedit maiores molestiae molestias nam necessitatibus nemo nihil nisi numquam odio odit
-                    officia quaerat quam qui, reiciendis repellat sed sint ullam vel voluptates. Ad animi, aspernatur
-                    consequatur consequuntur dicta, dignissimos dolores eaque eligendi et, eveniet ex illo in incidunt
-                    magni nam possimus quae quibusdam quidem quod recusandae similique sunt tempora? Aliquam aspernatur
-                    deleniti ducimus eaque earum fugiat, harum hic impedit iusto molestias non officia possimus
-                    quibusdam similique unde, vel voluptatum. Assumenda id iusto quis repudiandae ut. Incidunt, porro
-                    ut! Accusamus aliquid aut cumque distinctio eligendi expedita, ipsa magni maxime natus officiis
-                    porro provident, quae quam, quas quos rem ullam voluptatibus. Aliquid, amet consectetur consequatur
-                    culpa eos eum hic impedit, inventore ipsum itaque neque nulla quaerat recusandae rem saepe ullam
-                    vitae. Alias architecto delectus deserunt doloribus ducimus earum eligendi ex ipsum labore, minima
-                    mollitia necessitatibus nobis pariatur perferendis porro quaerat quas tenetur veniam! Accusamus
-                    aspernatur deserunt eaque facere nostrum quia sint tempora! Accusamus aliquam animi aperiam
-                    architecto consequatur corporis delectus deleniti distinctio enim eum eveniet, ex excepturi fugiat
-                    fugit id ipsa iste magni maiores minus mollitia praesentium provident, quidem quis recusandae
-                    repellat sit tempora voluptatibus? A adipisci animi assumenda at atque consequuntur dicta distinctio
-                    dolor doloribus dolorum enim hic id illo ipsa iste iusto laudantium libero minima nemo nesciunt non
-                    nostrum numquam officiis perferendis quae quidem quis quod ratione reiciendis repellendus suscipit,
-                    tempora vero voluptatum. Ad alias aperiam architecto asperiores culpa deleniti distinctio dolorem
-                    dolorum eaque eius eveniet in itaque modi nihil possimus praesentium provident quod quos rem
-                    repellendus sint soluta tempore tenetur unde, vitae voluptate voluptatem. Ad adipisci aspernatur
-                    consequuntur debitis earum, eum eveniet excepturi explicabo hic illum incidunt laboriosam libero
-                    maxime porro quo reiciendis repellendus repudiandae, sint tenetur veniam. Asperiores at eos error
-                    minus molestias obcaecati qui, repudiandae rerum soluta velit voluptate voluptatem. Amet autem
-                    beatae commodi eaque necessitatibus quam quas, quo repellat. Dolorum harum illo provident ut!
-                    Aspernatur facere nostrum sint unde? Aliquid at mollitia necessitatibus nesciunt perferendis placeat
-                    porro provident totam ullam voluptatibus. Accusamus adipisci aspernatur at aut doloremque ea esse,
-                    exercitationem molestiae possimus quibusdam tenetur voluptate voluptatem. A aspernatur consequatur
-                    eaque enim eveniet ex fugiat iure laboriosam magni nesciunt nobis non placeat repellat sed sit
-                    tempore, temporibus vel. Accusantium assumenda cum dolorum, eaque eligendi esse explicabo hic
-                    nostrum nulla perferendis quo reprehenderit temporibus velit veniam voluptas voluptatem
-                    voluptatibus? Aspernatur deleniti error eum libero, nemo nostrum odit possimus qui, rem similique
-                    tenetur ullam, voluptatibus! Accusamus assumenda hic ipsam nemo quos recusandae rem reprehenderit
-                    tempora temporibus. Ad, debitis ipsum magni mollitia quam repellat sit. Aliquid at doloribus eaque
-                    maiores natus vitae, voluptatum. Aut consectetur cumque ex facere ipsa labore odit placeat quae
-                    ullam vero! Aliquid beatae consectetur ducimus eligendi hic incidunt ipsam iste labore magnam minima
-                    nam pariatur, repellat sint temporibus, voluptate! Adipisci alias amet aperiam architecto atque
-                    consectetur consequatur corporis deleniti, deserunt dolor eaque est et facilis hic in inventore iste
-                    laborum laudantium magnam magni maiores molestias natus necessitatibus nihil nostrum nulla omnis
-                    placeat ratione recusandae repellat sed sequi tempora temporibus ullam veniam veritatis voluptate.
-                    Commodi doloremque ea esse quia quidem quisquam quod velit. Adipisci animi atque commodi distinctio
-                    eaque excepturi explicabo, facilis hic inventore labore natus optio quasi vero. A aperiam blanditiis
-                    cumque delectus doloremque dolorum error est et exercitationem illo impedit ipsa laborum, magnam
-                    maxime minima nam neque nulla odio officia omnis perferendis quam quidem recusandae soluta tempora
-                    tenetur ullam voluptas voluptate, voluptatem voluptates! Amet aperiam beatae consequatur corporis
-                    cum cumque dolor dolorem ea error est facere labore laudantium molestias mollitia non optio
-                    pariatur, perspiciatis porro, possimus quaerat qui sed similique soluta unde velit vero
-                    voluptatibus. Delectus deleniti doloremque exercitationem iusto nobis, placeat quia suscipit velit!
-                    Adipisci, aliquam aperiam aspernatur culpa cum cumque cupiditate deserunt distinctio eos eveniet
-                    facilis in inventore itaque libero natus necessitatibus nesciunt obcaecati placeat porro qui
-                    reiciendis, repellendus, sint soluta sunt ut vel veritatis voluptatum! A architecto, consectetur
-                    debitis ducimus facere itaque non obcaecati officia, perferendis porro quos reiciendis sequi! Ab
-                    asperiores assumenda, cumque distinctio excepturi exercitationem facere magnam, maxime neque qui
-                    veritatis voluptate. Ab assumenda deserunt dolorum esse minima minus optio porro quis ullam ut.
-                    Aliquam aperiam atque autem, culpa, debitis doloribus ducimus eligendi enim eos excepturi facilis,
-                    illum inventore ipsum nihil officia quisquam sed soluta. Animi aspernatur at atque aut cumque
-                    delectus distinctio dolor dolore dolores eaque esse fuga illo ipsam iure, iusto libero modi
-                    molestiae mollitia natus nobis nulla numquam obcaecati quam quia sed soluta ut vel. A ad animi
-                    beatae commodi corporis delectus deleniti dignissimos dolore eius magnam magni maxime, modi odio
-                    officia optio praesentium quaerat voluptatem voluptatum. Accusantium ad alias aliquam amet aperiam,
-                    aspernatur assumenda consectetur consequatur cupiditate deserunt dicta dolorem doloremque ducimus ea
-                    eius exercitationem in itaque minima minus nihil perspiciatis quidem repellat saepe sequi ullam
-                    veniam, voluptas voluptates? Asperiores assumenda at autem corporis debitis doloremque, dolores
-                    excepturi ipsum iste molestias nemo obcaecati quasi quis, quod quos vero voluptates? Delectus fugiat
-                    sit soluta veritatis. A ab cum exercitationem iusto laboriosam natus odit omnis quidem
-                    reprehenderit. Autem debitis deleniti, dignissimos dolorem eos error, eum facere facilis magni
-                    neque, nesciunt nisi obcaecati optio possimus provident repellat soluta.</h4>
+                </div>
+                <div className="section__join">
+                    <div className="join">
+                        <div className="join__picture">
+                            <img src={home_img} alt="pic"/>
+                        </div>
+                        <div className="join__text">
+
+                            <h3>О микродозинге</h3>
+
+
+                            <p>Мухоморный микродозинг – это употребление мухомора в малых количествах. Регулярное
+                                применение сушеных грибов оказывает положительное физиологическое и
+                                психотерапевтическое воздействие. Активные вещества этого лекарственного гриба
+                                благотворно влияют на организм и содержат ряд преимуществ для здоровья мозга и
+                                тела. </p>
+                            <p>При ежедневном приеме увеличивается умственная и физическая активность, становится
+                                проще обрабатывать большие объемы информации, сон становится крепким и здоровым,
+                                усталость накопленная за многие годы уходит. Также снижается тяга ко вредным
+                                привычкам (алкоголь, курение), появляется потребность в здоровой пище, мироощущение
+                                становится позитивным. Мухоморный микродозинг не вызывает привыкания и не требует
+                                перерыва между курсами.</p>
+                            <button className="btn ">Узнать больше</button>
+                        </div>
+
+                    </div>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cupiditate dolorum eos error hic
+                    inventore laboriosam minus, numquam officiis pariatur quam qui, quibusdam quis, rerum sequi voluptas
+                    voluptate. Accusamus aliquam assumenda deserunt dicta dolorum error harum hic illum, in ipsum itaque
+                    modi molestiae nam nobis obcaecati officiis perspiciatis placeat provident quibusdam quod rem saepe
+                    unde ut! Amet, autem beatae commodi culpa deserunt doloribus enim excepturi fugit illo incidunt
+                    ipsam iste iusto labore maxime minima nemo nihil nostrum provident rem sunt tempora ut vero? A
+                    aperiam, ea eligendi fugit itaque laboriosam magni maiores molestiae nemo omnis optio porro quas
+                    repudiandae ut!
+                </div>
+
             </div>
         </motion.div>
-)
+    )
 }
