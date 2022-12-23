437cb61090f58d03a569be321c1b8f1167f22679
Karina <vbank322@mail.ru>
2022-12-23 17:44:41 +0300

    fix header

--- src/pages/Products/components/ProductsItem/ProductItem.tsx
+++ src/pages/Products/components/ProductsItem/ProductItem.tsx
@@ -1,6 +1,5 @@
-import React, {FC, useEffect, useState} from 'react';
+import {FC, useEffect, useState} from 'react';
 import {IProduct} from "../../../../types";
-// @ts-ignore
 import CashImage from '../../../../assets/icons/Shop/money.svg'
 import {Counter} from "../Counter/ Counter";
 import {useDispatch} from "react-redux";
