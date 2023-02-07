3e0320f4774675157ce01a80ef82b1095e32832a
Karina <vbank322@mail.ru>
2023-02-07 22:22:59 +0300

    fix card basket

--- src/hooks/UseValidation.tsx
+++ src/hooks/UseValidation.tsx
@@ -4,51 +4,45 @@ export const useValidation = (value: any, validations: any) => {
 
     const [isEmpty, setEmpty] = useState(true)
     const [minLengthError, setMinLengthError] = useState(false)
-    const [isPhone, setPhone] = useState(false)
-    const [nameError, setNameError] = useState(false)
-    const [PhoneError, setPhoneError] = useState(false)
+    const [phoneError, setPhoneError] = useState(false)
     const [inputValid, setInputValid] = useState(false)
 
 
     useEffect(() => {
         for (const validation in validations) {
             switch (validation) {
-                case 'minLength':
-                    value.length < validations[validation] ? setMinLengthError(true) : setMinLengthError(false)
-                    break;
+
                 case 'isEmpty':
                     value ? setEmpty(false) : setEmpty(true)
                     break;
-                case 'Phone':
-                    value.length != validations[validation] ? setPhone(true) : setPhone(false)
+
+                case 'minLength':
+                    value.length < validations[validation] ? setMinLengthError(true) : setMinLengthError(false)
                     break;
-                case 'testName':
-                    const reg = /[^a-zа-яё\s]/gi;
-                    reg.test(String(value).toLowerCase()) ? setNameError(true) : setNameError(false)
-                    break;
-                case 'testPhone':
-                    const reg2 = /[^0-9]/g;
-                    reg2.test(String(value).toLowerCase()) ? setPhoneError(true) : setPhoneError(false)
+
+                case 'testPhone' :
+                    const replacePhone = value.replace(/[^0-9,.]/g, '');
+                    replacePhone.length === 12 ? setPhoneError(false) : setPhoneError(true)
                     break;
             }
         }
     }, [value])
 
+
     useEffect(() => {
-        if (isEmpty || minLengthError || isPhone || nameError || PhoneError) {
+        if (minLengthError || phoneError) {
             setInputValid(false)
         } else {
             setInputValid(true)
         }
-    }, [isEmpty, minLengthError, isPhone, nameError, PhoneError])
+
+    }, [isEmpty, minLengthError, phoneError])
 
     return {
         isEmpty,
         minLengthError,
-        isPhone,
         inputValid,
-        nameError,
-        PhoneError
+        phoneError
     }
 }
 
--- src/shared/UI/Cart/Cart.tsx
+++ src/shared/UI/Cart/Cart.tsx
@@ -9,6 +9,8 @@ import {ItemCard} from "./ItemCard";
 import {Modal} from "../Modal/Modal";
 import CashIco from '../../../assets/icons/Shop/money.svg'
 import {useInput} from "../../../hooks/useInput";
+import {CardForm} from "./CardForm/CardForm";
+
 
 
 
@@ -18,9 +20,9 @@ interface Cart {
 
 const Cart: FC<Cart> = ({}) => {
 
-    const nameInputHooks = useInput('', {isEmpty: true, minLength: 2, testName: true})
-    const telInputHooks = useInput('', {isEmpty: true, testPhone: true, minLength: 12})
-
+    // const nameInputHooks = useInput('', {isEmpty: true, minLength: 2, testName: true})
+    // const telInputHooks = useInput('', {isEmpty: true, testPhone: true, minLength: 12})
+    //
     const [name, setName] = useState('');
 
     const [number, setNumber] = useState('');
@@ -37,25 +39,25 @@ const Cart: FC<Cart> = ({}) => {
             acc + item.finalPrice
         , 0)
 
-    //ts-ignore
-    useEffect(() => {
-        if (!name || !number) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-        }
-    }, [name, number])
+
+    // useEffect(() => {
+    //     if (!name || !number) {
+    //         tg.MainButton.hide();
+    //     } else {
+    //         tg.MainButton.show();
+    //     }
+    // }, [name, number])
 
 
-    const onChangeName = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-        setName(e.target.value)
-        nameInputHooks.onChange(e)
-    }
-
-    const onChangeNumber = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-        setNumber(e.target.value)
-        telInputHooks.onChange(e)
-    }
+    // const onChangeName = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+    //     setName(e.target.value)
+    //     nameInputHooks.onChange(e)
+    // }
+    //
+    // const onChangeNumber = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+    //     setNumber(e.target.value)
+    //     telInputHooks.onChange(e)
+    // }
 
 
     const dispatch = useDispatch()
@@ -120,7 +122,6 @@ const Cart: FC<Cart> = ({}) => {
         const URL_API_2 = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatIdMark}&text=${message}&parse_mode=html`;
         let api = new XMLHttpRequest();
         let api1 = new XMLHttpRequest();
-        // @ts-ignore
         api.open("GET", URL_API_1, true);
         api1.open("GET", URL_API_2, true);
         api.send();
@@ -135,76 +136,99 @@ const Cart: FC<Cart> = ({}) => {
 
 
     return (
-        <div className="cart">
-                <div className='cart__data--content'>
-                    <h3 className='cart__data--content-title'>Корзина</h3>
-                    {(cart.length > 0) &&
-                        <div className='cart__data--content-items'>
-                        {cart.map((item, key) => <ItemCard key={cart.indexOf(item)} item={item} removeHandler={removeHandler}/>)}
-                    </div>}
-                    {(cart.length === 0 && !submitTg) && <>
-                        <div className='cart__data--content-no-item'>Ваша корзина пуста!</div>
-                    </>}
-                    {submitTg && <div className='cart__data--content-form-submit'>
-                        <div className='cart__data--content-form-submit-title'>Спасибо за заказ!</div>
-                        <div className='cart__data--content-form-submit-subtitle'>В ближайшее время менеджер свяжется с
-                            вами для подтверждения заказа!
-                        </div>
-                        <div className="cart__data--content-form-submit-line-wrapper">
-                        <motion.div
-                            className='cart__data--content-form-submit-line'
-                            initial={{width: "100%"}}
-                            animate={{width: "1%"}}
-                            transition={{duration: 10}}
-                        />
-                        </div>
-                    </div>}
-                    {!submitTg && <div className='cart__data--content-total'>
-                        <div className='cart__data--content-total-ico'><img src={CashIco}/></div>
-                        <div className='cart__data--content-total-summ'>Общая
-                            сумма: <b>{Math.round(total)} BYN</b>
-                        </div>
-                    </div>}
-                    {(cart.length > 0) && <div className='form'>
-                        <div className='cart__data--content-form'>
-                            <div className='cart__data--content-form-title'>Введите ваши данные:</div>
-                            {(nameInputHooks.isDirty && nameInputHooks.isEmpty) &&
-                                <div style={{color: 'red'}}>Поле не может быть пустым</div>}
-                            {(nameInputHooks.isDirty && nameInputHooks.minLengthError) &&
-                                <div style={{color: 'red'}}>Поле должно содержать более 2 символов</div>}
-                            {(nameInputHooks.isDirty && nameInputHooks.nameError) &&
-                                <div style={{color: 'red'}}>Можно вводить только буквы</div>}
-                            <input className='cart__data--content-form-input-name'
-                                   type='text'
-                                   placeholder='Имя'
-                                   value={nameInputHooks.value}
-                                   onBlur={e => nameInputHooks.onBlur(e)}
-                                   onChange={onChangeName}
-                            />
-                            {(telInputHooks.isDirty && telInputHooks.isEmpty) &&
-                                <div style={{color: 'red'}}>Поле не может быть пустым</div>}
-                            {(telInputHooks.isDirty && telInputHooks.PhoneError) &&
-                                <div style={{color: 'red'}}>Можно вводить только цифры</div>}
-                            {(telInputHooks.isDirty && telInputHooks.minLengthError) &&
-                                <div style={{color: 'red'}}>Напишите номер в формате 375ххххххххх</div>}
-                            <input className='cart__data--content-form-input-tel'
-                                   type='tel'
-                                   maxLength={13}
-                                   placeholder='Номер телефона'
-                                   value={telInputHooks.value}
-                                   onBlur={e => telInputHooks.onBlur(e)}
-                                   onChange={onChangeNumber}
-                            />
-                        </div>
-                        <div className='cart__data--content-button'>
-                            <button disabled={!telInputHooks.inputValid || !nameInputHooks.inputValid}
-                                    className={(!telInputHooks.inputValid || !nameInputHooks.inputValid) ? 'cart__data--content-button-telegram  disabled' : 'cart__data--content-button-telegram'}
-                                    onClick={onSendData}>Оформить
-                                заказ
-                            </button>
-                        </div>
-                    </div>}
-                </div>
+        <div className="cards">
+            <h3 className='cards__title'>Корзина</h3>
+
+
+            <div className='cards-list'>
+                {cart.map((item, key) =>
+                    <ItemCard item={item} removeHandler={removeHandler} key={item.id}/>
+                )}
+            </div>
+
+
+            {(cart.length === 0 && !submitTg) && <span className='cards__empty'>Ваша корзина пуста!</span>}
+
+
+            {!submitTg && <div className='cards-total'>
+                <img src={CashIco}/>
+                <span>Общая сумма:</span> <span> {Math.round(total)} BYN</span>
+            </div>}
+
+
+            {(cart.length > 0) &&  <CardForm/>}
+
+
+
+                {/*<div className='cart__data--content'>*/}
+                {/*    <h3 className='cart__data--content-title'>Корзина</h3>*/}
+                {/*    {(cart.length > 0) &&*/}
+                {/*        <div className='cart__data--content-items'>*/}
+                {/*        {cart.map((item, key) => <ItemCard key={cart.indexOf(item)} item={item} removeHandler={removeHandler}/>)}*/}
+                {/*    </div>}*/}
+                {/*    {(cart.length === 0 && !submitTg) && <>*/}
+                {/*        <div className='cart__data--content-no-item'>Ваша корзина пуста!</div>*/}
+                {/*    </>}*/}
+                {/*    {submitTg && <div className='cart__data--content-form-submit'>*/}
+                {/*        <div className='cart__data--content-form-submit-title'>Спасибо за заказ!</div>*/}
+                {/*        <div className='cart__data--content-form-submit-subtitle'>В ближайшее время менеджер свяжется с*/}
+                {/*            вами для подтверждения заказа!*/}
+                {/*        </div>*/}
+                {/*        <div className="cart__data--content-form-submit-line-wrapper">*/}
+                {/*        <motion.div*/}
+                {/*            className='cart__data--content-form-submit-line'*/}
+                {/*            initial={{width: "100%"}}*/}
+                {/*            animate={{width: "1%"}}*/}
+                {/*            transition={{duration: 10}}*/}
+                {/*        />*/}
+                {/*        </div>*/}
+                {/*    </div>}*/}
+                    {/*{!submitTg && <div className='cart__data--content-total'>*/}
+                    {/*    <div className='cart__data--content-total-ico'><img src={CashIco}/></div>*/}
+                    {/*    <div className='cart__data--content-total-summ'>Общая*/}
+                    {/*        сумма: <b>{Math.round(total)} BYN</b>*/}
+                    {/*    </div>*/}
+                    {/*</div>}*/}
+                    {/*{(cart.length > 0) && <div className='form'>*/}
+                    {/*    <div className='cart__data--content-form'>*/}
+                    {/*        <div className='cart__data--content-form-title'>Введите ваши данные:</div>*/}
+                    {/*        {(nameInputHooks.isDirty && nameInputHooks.isEmpty) &&*/}
+                    {/*            <div style={{color: 'red'}}>Поле не может быть пустым</div>}*/}
+                    {/*        {(nameInputHooks.isDirty && nameInputHooks.minLengthError) &&*/}
+                    {/*            <div style={{color: 'red'}}>Поле должно содержать более 2 символов</div>}*/}
+                    {/*        {(nameInputHooks.isDirty && nameInputHooks.nameError) &&*/}
+                    {/*            <div style={{color: 'red'}}>Можно вводить только буквы</div>}*/}
+                    {/*        <input className='cart__data--content-form-input-name'*/}
+                    {/*               type='text'*/}
+                    {/*               placeholder='Имя'*/}
+                    {/*               value={nameInputHooks.value}*/}
+                    {/*               onBlur={e => nameInputHooks.onBlur(e)}*/}
+                    {/*               onChange={onChangeName}*/}
+                    {/*        />*/}
+                    {/*        {(telInputHooks.isDirty && telInputHooks.isEmpty) &&*/}
+                    {/*            <div style={{color: 'red'}}>Поле не может быть пустым</div>}*/}
+                    {/*        {(telInputHooks.isDirty && telInputHooks.PhoneError) &&*/}
+                    {/*            <div style={{color: 'red'}}>Можно вводить только цифры</div>}*/}
+                    {/*        {(telInputHooks.isDirty && telInputHooks.minLengthError) &&*/}
+                    {/*            <div style={{color: 'red'}}>Напишите номер в формате 375ххххххххх</div>}*/}
+                    {/*        <input className='cart__data--content-form-input-tel'*/}
+                    {/*               type='tel'*/}
+                    {/*               maxLength={13}*/}
+                    {/*               placeholder='Номер телефона'*/}
+                    {/*               value={telInputHooks.value}*/}
+                    {/*               onBlur={e => telInputHooks.onBlur(e)}*/}
+                    {/*               onChange={onChangeNumber}*/}
+                    {/*        />*/}
+                    {/*    </div>*/}
+                    {/*    <div className='cart__data--content-button'>*/}
+                    {/*        <button disabled={!telInputHooks.inputValid || !nameInputHooks.inputValid}*/}
+                    {/*                className={(!telInputHooks.inputValid || !nameInputHooks.inputValid) ? 'cart__data--content-button-telegram  disabled' : 'cart__data--content-button-telegram'}*/}
+                    {/*                onClick={onSendData}>Оформить*/}
+                    {/*            заказ*/}
+                    {/*        </button>*/}
+                    {/*    </div>*/}
+                    {/*</div>}*/}
+                {/*</div>*/}
         </div>
     )
 }
